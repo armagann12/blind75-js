cf7559f63d1f7c320357a97c26e2848ced27117b
armagann12 <armagansisik@gmail.com>
2023-06-29 18:36:23 +0300

    init commit

---
+++ array/two-sum.js
@@ -0,0 +1,19 @@
+// Using nested for loop
+var twoSum = function (nums, target) {
+    var arr = []
+    for (var i = 0; i < nums.length - 1; i++) {
+        for (var j = 1; j < nums.length; j++) {
+            if(nums[i] + nums[j] === target) {
+                arr.push(i, j)
+                return arr
+            }
+        }
+    }
+    return arr
+};
+
+var nums = [2, 7, 11, 15]
+var target = 22
+
+var res = twoSum(nums, target)
+console.log(res)

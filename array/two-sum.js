36c92e47d4869ba86c8d1f45eb5d05a6c592a430
armagann12 <armagansisik@gmail.com>
2023-06-29 19:32:36 +0300

    two-sum implemented optimal solution

--- array/two-sum.js
+++ array/two-sum.js
@@ -1,4 +1,5 @@
 // Using nested for loop
+// O(n^2)
 var twoSum = function (nums, target) {
     var arr = []
     for (var i = 0; i < nums.length - 1; i++) {
@@ -12,8 +13,27 @@ var twoSum = function (nums, target) {
     return arr
 };
 
+// Using a hash map (One pass solution) --> !! Most optimal solution we dont initialize hashmap we look as we go 
+// O(n)
+
+var twoSumAlt = function (nums, target) {
+    arr = []
+    map = {}
+    for (var i = 0; i < nums.length; i++) {
+        var dif = target - nums[i]
+        if (map.hasOwnProperty(dif)){
+            return[map[dif], i]
+        }
+        map[nums[i]] = i
+    }
+    return arr
+}
+
 var nums = [2, 7, 11, 15]
 var target = 22
 
 var res = twoSum(nums, target)
 console.log(res)
+
+var resAlt = twoSumAlt(nums, target)
+console.log(resAlt, "onepass")

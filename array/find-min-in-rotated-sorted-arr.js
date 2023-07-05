7f3b4d93af834537dbba6167aed690b2556675c5
armagann12 <armagansisik@gmail.com>
2023-07-05 23:30:38 +0300

    find min in rotated sorted array optimal solution

--- array/find-min-in-rotated-sorted-arr.js
+++ array/find-min-in-rotated-sorted-arr.js
@@ -27,7 +27,27 @@ var findMinAlt = (nums) => {
     return min
 };
 
+// O(log n) solution Binary Search
 
+var findMin = (nums) => {
+    var min = nums[0]
+    var left = 0
+    var right = nums.length - 1
+    while (left <= right) {
+        if (nums[left] < nums[right]) {
+            min = Math.min(min, nums[left])
+            break
+        }
+        var middle = Math.round((left + right) / 2)
+        min = Math.min(min, nums[middle])
+        if (nums[left] >= nums[middle]) {
+            right = middle - 1
+        } else {
+            left = middle + 1
+        }
+    }
+    return min
+};
 
 var nums = [4, 5, 6, 7, 0, 1, 2]
 

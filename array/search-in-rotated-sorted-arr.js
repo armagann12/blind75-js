c6e95408aa83f5d2a499c8e2673eb0470aae2b9c
armagann12 <armagansisik@gmail.com>
2023-07-06 18:28:49 +0300

    search ing rotated sorted arr binary search solution

--- array/search-in-rotated-sorted-arr.js
+++ array/search-in-rotated-sorted-arr.js
@@ -4,17 +4,55 @@
 // O(n) solution using a for loop
 var search = (nums, target) => {
     var res = -1
-    for(var i = 0; i<nums.length; i++){
-        if (nums[i] === target){
+    for (var i = 0; i < nums.length; i++) {
+        if (nums[i] === target) {
             res = i
         }
     }
     return res
 };
 
+// O(log n) Bianry search using comparison logic
+// My solution
+var searchAlt = (nums, target) => {
+    var res = -1
+    var left = 0
+    var right = nums.length - 1
+    while (left <= right) {
+        var mid = Math.round((left + right) / 2)
+        if (nums[mid] === target) {
+            return mid
+        } else if (nums[left] === target) {
+            return left
+        } else if (nums[right] === target) {
+            return right
+        }
+        console.log(left, right, mid)
+        if (nums[left] > nums[mid]) {
+            if (target > nums[left]) {
+                right = mid - 1
+            } else if (target < nums[mid]) {
+                right = mid - 1
+            }
+            else {
+                left = mid + 1
+            }
+        } else {
+            if (target < nums[left]) {
+                left = mid + 1
+            } else if (target > nums[mid]) {
+                left = mid + 1
+            } else {
+                right = mid - 1
+            }
+        }
+    }
+    return res
+}
 
-var nums = [4, 5, 6, 7, 0, 1, 2]
-var target = 0
 
-var res = search(nums, target)
+var nums = [7, 8, 1, 2, 3, 4, 5, 6]
+var target = 2
+
+var res = searchAlt(nums, target)
 console.log(res)

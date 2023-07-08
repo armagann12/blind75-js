76816b2f6738d565488c3a5047cb550cd1173e1f
armagann12 <armagansisik@gmail.com>
2023-07-08 19:35:55 +0300

    three-sum two-sum approach

--- array/three-sum.js
+++ array/three-sum.js
@@ -1,9 +1,11 @@
 // 15. 3Sum
 // https://leetcode.com/problems/3sum/
 
+// Also solve twoSum II
+
 // Brute solution using 3 loops + Also we sort the array first so we can see duplicates and filter
 // O(n^3)
-// Leetcode not passed for some reason?
+// Leetcode not passed because [0,0,0,0]
 var threeSum = (nums) => {
     var arr = []
     var sortedNums = nums.sort() // Do it also with sorting algorithms
@@ -24,7 +26,34 @@ var threeSum = (nums) => {
     return arr
 };
 
-var nums = [0, 0, 0]
+// Using one element and doing two sum with hash map for the rest
+// O (n^2)
+// Leetcode not passed because [0,0,0,0]
+var threeSumAlt = (nums) => {
+    var arr = []
+    var sortedNums = nums.sort()
 
-var res = threeSum(nums)
+    for (var i = 0; i < sortedNums.length - 1; i++) {
+        var map = {}
+        if (i > 0 && sortedNums[i] === sortedNums[i - 1]) {
+            continue
+        }
+        var target = 0 - sortedNums[i]
+        for (var j = i + 1; j < sortedNums.length; j++) {
+            var dif = target - sortedNums[j]
+            if (map.hasOwnProperty(dif)) {
+                var tmpArr = [sortedNums[i], sortedNums[map[dif]], sortedNums[j]]
+                arr.push(tmpArr)
+            }
+            map[sortedNums[j]] = j
+        }
+    }
+    return arr
+}
+
+var nums = [-2, 0, 0, 2, 2]
+
+
+
+var res = threeSumAlt(nums)
 console.log(res)

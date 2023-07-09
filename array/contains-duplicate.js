10672e04c925a6430c65c6f9eb841048d872f5f7
armagann12 <armagansisik@gmail.com>
2023-07-09 20:33:07 +0300

    contains-duplicate :two_hearts:

--- array/contains-duplicate.js
+++ array/contains-duplicate.js
@@ -1,6 +1,6 @@
 // 217. Contains Duplicate
 // https://leetcode.com/problems/contains-duplicate/
-
+// Done
 
 // Using nested for loops (Brute force)
 // O(n^2)
@@ -8,7 +8,7 @@
 var containsDuplicate = function (nums) {
     for (var i = 0; i < nums.length - 1; i++) {
         for (var j = i + 1; j < nums.length; j++) {
-            if(nums[i] === nums[j]){
+            if (nums[i] === nums[j]) {
                 return true
             }
         }
@@ -19,13 +19,13 @@ var containsDuplicate = function (nums) {
 //Using a hashmap
 // O(n)
 // Memory O(n)
-var containsDuplicateAlt = (nums) =>{
+var containsDuplicateAlt = (nums) => {
     map = {}
-    for(var i in nums){
+    for (var i in nums) {
         // We can also check map.hasOwnProperty(nums[i])
-        if(map[nums[i]] === 1){
+        if (map[nums[i]] === 1) {
             return true
-        }else{
+        } else {
             map[nums[i]] = 1
         }
     }
@@ -36,6 +36,16 @@ var containsDuplicateAlt = (nums) =>{
 // Sorting the array and using just one for loop checking if side by side numbers is equal
 // O(nlogn) -> Because of sorting
 
+var containsDuplicateAlt2 = (nums) => {
+    nums.sort()
+    for (var i = 0; i < nums.length - 1; i++){
+        if(nums[i] === nums[i + 1]){
+            return true
+        }
+    }
+    return false
+}
+
 var nums = [1, 2, 3, 1]
 var res = containsDuplicate(nums)
 
@@ -44,3 +54,7 @@ console.log(res, "res")
 var resAlt = containsDuplicateAlt(nums)
 
 console.log(resAlt)
+
+var resAlt2 = containsDuplicateAlt2(nums)
+
+console.log(resAlt2)

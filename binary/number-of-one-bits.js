cd52c4b7ffc72bd6b5b60700b3f9e74bcdf2da54
armagann12 <armagansisik@gmail.com>
2023-07-11 14:00:18 +0300

    number-of-one-bits fixed earlier issue

--- binary/number-of-one-bits.js
+++ binary/number-of-one-bits.js
@@ -5,27 +5,25 @@
 
 // Doing & operation with 1 and increment res if it returns 1 --> 1010 & 0001 will return 0 + 0101 & 0001 will return 1
 // Than shifting the binary to right
-// Wont pass leetcode
 var hammingWeight = function (n) {
     var res = 0
-    while (n > 0) {
+    while (n !== 0) {
         var one = n & 1
         if (one === 1) {
             res++
         }
-        n = n >> 1 // --> Also dividing by 2 shifts the element by one
+        n = n >>> 1 // --> Also dividing by 2 shifts the element by one
     }
     return res
 };
 
 // Getting the mod of 2 of the number --> if its 0 it will add 0 so none + if its 1 it will be 1 so add 1
 // Than shifting the binary to right
-// Wont pass leetcode
 var hammingWeight2 = function (n) {
     var res = 0
     while (n > 0) {
         res += n % 2
-        n = n >> 1
+        n = n >>> 1 // -> Not >> it should be >>>
     }
     return res
 }
@@ -43,6 +41,6 @@ var hammingWeight3 = function (n) {
     return res
 }
 
-var n = 128
-var res = hammingWeight3(n)
+var n = 4294967293
+var res = hammingWeight(n)
 console.log(res)

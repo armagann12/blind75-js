f12df84506e2dc98fc0a697a46a52cd9da09faf9
armagann12 <armagansisik@gmail.com>
2023-07-11 16:58:46 +0300

    missing-numbers binary optimal solution

--- binary/missing-number.js
+++ binary/missing-number.js
@@ -15,12 +15,24 @@ var missingNumber = (nums) => {
 // Another solution without bitwise
 var missingNumber2 = (nums) => {
     var res = 0
-    while(nums.includes(res)){
+    while (nums.includes(res)) {
         res++
     }
     return res
 }
 
+// Binary solution O(N) runtime O(1) space
+// With XOR --> 0 1 2 3 XOR with 0 1 3 will return 2 because same numbers will gives us 0000
+
+var missingNumber3 = (nums) => {
+    var res = nums.length
+    for (var i = 0; i < nums.length; i++) {
+        var xor = i ^ nums[i]
+        res = res ^ xor
+    }
+    return res
+}
+
 var nums = [3, 0, 1]
-var res = missingNumber2(nums)
+var res = missingNumber3(nums)
 console.log(res)

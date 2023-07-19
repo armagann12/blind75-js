98d222c283eb4c480e7f765175bbad5109d7bce6
armagann12 <armagansisik@gmail.com>
2023-07-19 17:56:21 +0300

    max-depth-of-binary-tree bfs solution

--- tree/maximum-depth-of-binary-tree.js
+++ tree/maximum-depth-of-binary-tree.js
@@ -8,12 +8,34 @@
 
 // DFS Recursive
 // O(n)
-var maxDepth = function(root) {
-    if(root === null){
+var maxDepth = function (root) {
+    if (root === null) {
         return 0
     }
     return 1 + Math.max(maxDepth(root.right), maxDepth(root.left))
 };
 
 
-// BFS using queue / (dequeu)
+// BFS using Queue
+// O(n)
+var maxDepth = function (root) {
+    if (root === null) {
+        return 0
+    }
+    var queue = [root]
+    var level = 0
+    while (queue.length !== 0) {
+        level++
+        var len = queue.length
+        for (var i = 0; i < len; i++) {
+            var node = queue.shift()
+            if (node.left !== null) {
+                queue.push(node.left)
+            }
+            if (node.right !== null) {
+                queue.push(node.right)
+            }
+        }
+    }
+    return level
+};

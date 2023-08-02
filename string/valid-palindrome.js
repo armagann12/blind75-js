0ebf07eb0c7c63ab286f843fddd37391dd9e5335
armagann12 <armagansisik@gmail.com>
2023-08-02 12:16:14 +0300

    valid-palindrome solution using two-pointer

--- string/valid-palindrome.js
+++ string/valid-palindrome.js
@@ -11,3 +11,26 @@ var isPalindrome = function(s) {
     }
     return newStr === newStr.split("").reverse().join("")
 };
+
+// Using two pointers and regex
+var isPalindrome = function (s) {
+    var l = 0
+    var r = s.length - 1
+    while (l < r) {
+        console.log(s[r])
+        var leftChar = s[l]
+        var rightChar = s[r]
+        if (!(leftChar.match(/^[A-Za-z0-9]+$/))) {
+            l++
+        } else if (!(rightChar.match(/^[A-Za-z0-9]+$/))) {
+            r--
+        } else {
+            if (leftChar.toLowerCase() !== rightChar.toLowerCase()) {
+                return false;
+            }
+            l++
+            r--
+        }
+    }
+    return true
+};

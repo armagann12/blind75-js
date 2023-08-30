189f409d52600185b48de281eb73b5f3d4251fe6
armagann12 <armagansisik@gmail.com>
2023-08-30 20:32:07 +0300

    top-k-frequent-elements extra solutions

--- heap/top-k-frequent-elements.js
+++ heap/top-k-frequent-elements.js
@@ -22,3 +22,29 @@ var topKFrequent = function (nums, k) {
     }
     return res
 };
+
+// My hashmap solution but refactored version
+var topKFrequent2 = function(nums, k) {
+    // results array
+    let results = [];
+    
+    // 1) first step is to build a hash map, where "element -> its frequency"
+    // it costs O(n), where n is nums.length
+    let map = {};
+    nums.forEach(n => map[n] ? map[n] += 1 : map[n] = 1);
+    
+    // 2) sort the map keys array based on its frequency
+    // it costs O(n log n), where n is nums.length
+    let sortedKeys = Object.keys(map).sort((a,b)=>map[b]-map[a]);
+    
+    // 3) take first k results
+    for(let i = 0; i < k; i++){
+        results.push(sortedKeys[i]);
+    }
+    
+    // as result we have O(n Log n) where n is length of nums
+    return results;
+};
+
+// Using bucket sort
+// https://www.youtube.com/watch?v=YPTqKIgVk-k&t=235s

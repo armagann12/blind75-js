829420c5a44e4e401824fc4a9efaa24d9a0701da
armagann12 <armagansisik@gmail.com>
2023-09-07 23:26:12 +0300

    longest-inc-subsequence py dp solution

---
+++ dynamic-programming/longest-increasing-subsequence.py
@@ -0,0 +1,12 @@
+# 300. Longest Increasing Subsequence
+# https://leetcode.com/problems/longest-increasing-subsequence/
+
+class Solution:
+    def lengthOfLIS(self, nums):
+        LIS = [1] * len(nums)
+
+        for i in range(len(nums) - 1, -1, -1):
+            for j in range(i + 1, len(nums)):
+                if nums[i] < nums[j]:
+                    LIS[i] = max(LIS[i], 1 + LIS[j])
+        return max(LIS)

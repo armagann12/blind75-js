54ce0a6d1382e249a85ec0df56f241ecb8cb3175
armagann12 <armagansisik@gmail.com>
2023-09-15 20:47:59 +0300

    house-robber py dp solution

---
+++ dynamic-programming/house-robber.py
@@ -0,0 +1,12 @@
+# 198. House Robber
+# https://leetcode.com/problems/house-robber
+
+class Solution(object):
+    def rob(self, nums):
+        rob1, rob2 = 0, 0
+
+        for n in nums:
+            temp = max(n + rob1, rob2)
+            rob1 = rob2
+            rob2 = temp
+        return rob2

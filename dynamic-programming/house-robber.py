f8c9b82a5e68db4268f86bfa455ce264c01101ad
armagann12 <armagansisik@gmail.com>
2023-09-15 20:48:49 +0300

    Added comment

--- dynamic-programming/house-robber.py
+++ dynamic-programming/house-robber.py
@@ -1,6 +1,7 @@
 # 198. House Robber
 # https://leetcode.com/problems/house-robber
 
+# Important question for DP in general
 class Solution(object):
     def rob(self, nums):
         rob1, rob2 = 0, 0
